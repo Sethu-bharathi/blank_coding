585e1ca41fe5f016806fdf366a037da1c5d247bd
Sethu-bharathi <bharathisethu018@gmail.com>
2022-04-29 23:19:04 +0530

    added logan loosu's component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 
 import './App.css';
-import Complaint from './components/Complaint/Complaint';
-import SignupHospital from './components/SignupHospital/signupHospital';
-import LoginHospital from './components/LoginHospital/Loginhospital';
-import Feedback from './components/Feedback/feedback';
-import HospitalProfile from './components/HospitalProfile/hospitalprofile';
-import Billing from './components/Billing/Billing';
-import PatientRegister from './components/Patientregister/PatientRegister';
+import HospitalManagement from './components/Hospital management/HospitalManagement';
 
 function App() {
   
   return (
     <div>
-      <PatientRegister />
+      <HospitalManagement />
     </div>
   );
 }
---
+++ src/components/Hospital management/HospitalManagement.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./HospitalManagement.css"
+export default function HospitalManagement(){
+    return(
+    <div className="page">
+        <div className="left-page">
+            <h5>HospitalName</h5>
+            <h5>Dean name</h5>
+            <h5>Dean Phonenumber</h5>
+            <h5>Email</h5>
+            <h5>Location</h5>
+            <h5>PhoneNumber</h5>
+        </div>
+        <div className="right-page">
+        <h5>HospitalName</h5>
+            <h5>Dean name</h5>
+            <h5>Dean Phonenumber</h5>
+            <h5>Email</h5>
+            <h5>Location</h5>
+            <h5>PhoneNumber</h5>
+        </div>
+    </div>);
+}
